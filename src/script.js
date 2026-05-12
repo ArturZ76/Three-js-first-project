218a2ad3fc9bbee65f79fb0b250f1034ddbbdba5
Artur Zajączkowski <artur.zajaczkowski76@gmail.com>
2026-05-12 16:05:26 +0200

    antialiasing

--- src/script.js
+++ src/script.js
@@ -38,9 +38,12 @@ camera.position.z = 5;
 // initialize the renderer
 const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRatio);
 
 
 //initialize the conrtrols
